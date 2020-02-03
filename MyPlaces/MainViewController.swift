0846bbda04eb17b9df38cec0b0043692d4fe72cc
Dmitry <ecawd>
2020-02-03 16:45:56 +0300

    skruglenie, Schrift, icon appicon,launchpad

--- MyPlaces/MainViewController.swift
+++ MyPlaces/MainViewController.swift
@@ -28,8 +28,13 @@ class MainViewController: UITableViewController
         
         cell.textLabel?.text = restaurantNames[indexPath.row]
         cell.imageView?.image = UIImage(named: restaurantNames[indexPath.row])
+        cell.imageView?.layer.cornerRadius = cell.frame.size.height / 2
+        cell.imageView?.clipsToBounds = true
         
         return cell
+    }
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 85
     }
 }
 
