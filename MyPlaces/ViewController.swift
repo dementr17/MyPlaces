7559ac2dc0d2744aa17f26027a8bd54ac9634700
Dmitry <ecawd>
2020-02-03 12:47:32 +0300

    Initial Commit

---
+++ MyPlaces/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyPlaces
+//
+//  Created by Дмитрий Чепанов on 03.02.2020.
+//  Copyright © 2020 Дмитрий Чепанов. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
